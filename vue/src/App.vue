91c54bd49f7028f2638695fcba57932dfbe3b51f
BorisKlco <klco@pm.me>
2025-02-13 20:22:11 +0100

    navbar

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -4,10 +4,29 @@ import { RouterView } from 'vue-router'
 
 <template>
   <header class="pt-12 px-8 md:w-[90%] xl:w-[80%] mx-auto">
-    <div class="w-full bg-gray-200 rounded-md overflow-hidden">
-      <img src="@/assets/nav.jpg" class="object-cover hidden sm:block">
-      <div class="h-6 px-4 text-sm md:h-8 flex items-center">
-        <p>a</p>
+    <div class="w-full bg-gray-200 rounded-md overflow-hidden outline outline-gray-400 shadow-sm">
+      <img src="@/assets/nav.jpg" class="object-cover hidden sm:block outline outline-gray-400">
+      <div class="h-8 px-4 text-sm md:h-10 flex items-center justify-between">
+        <div class="flex items-center gap-1 cursor-pointer">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+          </svg>
+          <p>Username234</p>
+        </div>
+
+        <div class="flex items-center gap-4">
+          <span class="flex items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
+            </svg>Users</span>
+          <span class="flex items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z" />
+</svg>
+FAQ
+          </span>
+          <p>Link</p>
+        </div>
       </div>
     </div>
   </header>
