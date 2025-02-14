97a1514376c7f37f330ed1e57562499d6c0eace0
BorisKlco <klco@pm.me>
2025-02-14 17:43:23 +0100

    board

---
+++ vue/src/components/TheBoard.vue
@@ -0,0 +1,18 @@
+<script setup>
+</script>
+
+<template>
+  <div class="mt-6 outline outline-gray-400 rounded-md">
+    <div class="py-2 px-2 bg-gray-200 border-b-[1px] border-gray-400">
+      <h1 class="text-xl">Board Category</h1>
+    </div>
+    <div class="flex justify-between h-8 items-center px-2">
+      <p>Forum</p>
+      <div class="flex">
+        <p class="px-6 border-x border-gray-400">Forum</p>
+        <p class="px-6">Forum</p>
+        <p class="px-6 border-l border-gray-400">Forum</p>
+      </div>
+    </div>
+  </div>
+</template>
--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
+import TheBoard from '@/components/TheBoard.vue';
 import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main class="pt-4 px-4 max-w-[95%] xl:max-w-[1440px] mx-auto">
     <TheWelcome />
+    <TheBoard />
   </main>
 </template>
