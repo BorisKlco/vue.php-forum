806b94df3a0c713ef8df3b03dc6a41b281cf5220
BorisKlco <klco@pm.me>
2025-02-15 11:12:29 +0100

    table css

--- vue/src/components/TheWelcome.vue
+++ vue/src/components/TheWelcome.vue
@@ -1,25 +1,26 @@
 <script setup>
-import { onMounted, ref } from 'vue';
+import { onMounted, ref } from 'vue'
 
-let quote = ref('');
+let quote = ref('')
 
-onMounted(()=>{
+onMounted(() => {
   fetch('https://dummyjson.com/quotes/random')
-  .then(res => res.json())
-  .then(q => {
-    quote.value = q;
-  });
+    .then((res) => res.json())
+    .then((q) => {
+      quote.value = q
+    })
 })
 </script>
 
 <template>
- <div class="outline outline-gray-400 rounded-md bg-gray-200 shadow-sm">
-    <div v-if="quote" class="py-2 px-2 flex items-center space-x-2">
-      <h1 class="font-semibold text-gray-800">Daily Quote:</h1>
-      <p>"{{ quote.quote }}" - <span>{{ quote.author }}</span></p>
+  <div class="outline outline-gray-400 rounded-md bg-gray-200 shadow-sm">
+    <div v-if="quote" class="py-2 px-2 flex items-center justify-center">
+      <p>
+        "{{ quote.quote }}" - <span>{{ quote.author }}</span>
+      </p>
     </div>
-    <div v-else class="py-2 px-2 flex items-centerspace-x-2">
-      <h1 class=" text-gray-800">...</h1>
+    <div v-else class="py-2 px-2 flex items-center justify-center">
+      <h1 class="text-gray-800">Loading daily quote...</h1>
     </div>
- </div>
+  </div>
 </template>
